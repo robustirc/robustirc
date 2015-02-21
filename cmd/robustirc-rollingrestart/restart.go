ecca84725a6da754a9974675c9eb75e219bb529f
Michael Stapelberg <michael@stapelberg.de>
2015-02-21 20:25:54 +0100

    Move status/healthy functions into util/ (for #86)
    They will be used both by rollingrestart and localnet.

--- cmd/robustirc-rollingrestart/restart.go
+++ cmd/robustirc-rollingrestart/restart.go
@@ -2,20 +2,18 @@ package main
 
 import (
 	"crypto/sha256"
-	"encoding/json"
 	"flag"
 	"fmt"
 	"io"
-	"io/ioutil"
 	"log"
 	"net"
 	"net/http"
 	"os"
 	"strings"
-	"sync"
 	"time"
 
 	"github.com/robustirc/robustirc/robusthttp"
+	"github.com/robustirc/robustirc/util"
 	"github.com/stapelberg/glog"
 )
 
@@ -76,40 +74,6 @@ func resolveNetwork() []string {
 	return servers
 }
 
-type serverStatus struct {
-	Server string
-
-	State          string
-	Leader         string
-	Peers          []string
-	AppliedIndex   uint64
-	CommitIndex    uint64
-	LastContact    time.Time
-	ExecutableHash string
-}
-
-func getServerStatus(server string) (serverStatus, error) {
-	var status serverStatus
-	req, err := http.NewRequest("GET", fmt.Sprintf("https://%s/", server), nil)
-	if err != nil {
-		return status, err
-	}
-	req.Header.Set("Accept", "application/json")
-	resp, err := robusthttp.Client(*networkPassword).Do(req)
-	if err != nil {
-		return status, err
-	}
-	defer resp.Body.Close()
-
-	if resp.StatusCode != http.StatusOK {
-		return status, fmt.Errorf("Expected HTTP OK, got %v", resp.Status)
-	}
-
-	err = json.NewDecoder(resp.Body).Decode(&status)
-	ioutil.ReadAll(resp.Body)
-	return status, err
-}
-
 func quit(server string) error {
 	req, err := http.NewRequest("POST", fmt.Sprintf("https://%s/quit", server), nil)
 	if err != nil {
@@ -125,68 +89,7 @@ func quit(server string) error {
 	return nil
 }
 
-// ensureNetworkHealthy returns nil when all of the following is true:
-//  • all nodes are reachable
-//  • all nodes return the same leader
-//  • all nodes are either follower or leader (i.e. not candidate/initializing)
-//  • all follower nodes were recently contacted by the leader
-func ensureNetworkHealthy(servers []string) (map[string]serverStatus, error) {
-	var leader string
-	statusChan := make(chan serverStatus, len(servers))
-	errChan := make(chan error, len(servers))
-	var wg sync.WaitGroup
-	for _, server := range servers {
-		wg.Add(1)
-		go func(server string) {
-			defer wg.Done()
-			status, err := getServerStatus(server)
-			if err != nil {
-				errChan <- err
-				return
-			}
-			status.Server = server
-			statusChan <- status
-		}(server)
-	}
-	wg.Wait()
-	close(errChan)
-	close(statusChan)
-
-	statuses := make(map[string]serverStatus, len(servers))
-
-	for err := range errChan {
-		return statuses, err
-	}
-
-	for status := range statusChan {
-		statuses[status.Server] = status
-
-		// No error checking since this was _parsed_ from JSON, so it must be valid.
-		pretty, _ := json.MarshalIndent(status, "", "  ")
-		glog.Infof("%s\n", pretty)
-
-		if status.State != "Leader" && status.State != "Follower" {
-			return statuses, fmt.Errorf("Server %q in state %q, need Leader or Follower",
-				status.Server, status.State)
-		}
-		if leader == "" {
-			leader = status.Leader
-		} else if leader != status.Leader {
-			return statuses, fmt.Errorf("Server %q thinks %q is leader, others think %q is leader",
-				status.Server, status.Leader, leader)
-		}
-		if status.State == "Follower" && time.Since(status.LastContact) > 1*time.Second {
-			return statuses, fmt.Errorf("Server %q was last contacted by the leader at %v, which is over a second ago",
-				status.Server, status.LastContact)
-		}
-	}
-	if leader == "" {
-		return statuses, fmt.Errorf("There is no leader currently")
-	}
-	return statuses, nil
-}
-
-func allNodesUpdated(statuses map[string]serverStatus, binaryHash string) bool {
+func allNodesUpdated(statuses map[string]util.ServerStatus, binaryHash string) bool {
 	for _, status := range statuses {
 		if status.ExecutableHash != binaryHash {
 			return false
@@ -208,7 +111,7 @@ func main() {
 
 	servers := resolveNetwork()
 	log.Printf("Checking network health\n")
-	if statuses, err := ensureNetworkHealthy(servers); err != nil {
+	if statuses, err := util.EnsureNetworkHealthy(servers, *networkPassword); err != nil {
 		log.Fatalf("Aborting upgrade for safety: %v", err)
 	} else {
 		if allNodesUpdated(statuses, binaryHash) {
@@ -221,12 +124,12 @@ func main() {
 
 	for rtry := 0; rtry < 5; rtry++ {
 		for _, server := range servers {
-			var statuses map[string]serverStatus
+			var statuses map[string]util.ServerStatus
 			var err error
 
 			started := time.Now()
 			for time.Since(started) < *networkHealthTimeout {
-				statuses, err = ensureNetworkHealthy(servers)
+				statuses, err = util.EnsureNetworkHealthy(servers, *networkPassword)
 				if err != nil {
 					log.Printf("Network is not healthy: %v\n", err)
 					time.Sleep(1 * time.Second)
@@ -253,7 +156,7 @@ func main() {
 
 			for htry := 0; htry < 60; htry++ {
 				time.Sleep(1 * time.Second)
-				current, err := getServerStatus(server)
+				current, err := util.GetServerStatus(server, *networkPassword)
 				if err != nil {
 					log.Printf("Node %q unhealthy: %v\n", server, err)
 					continue
