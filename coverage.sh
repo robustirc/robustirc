d8d93522767bdff19957a99ce0c992e9e286d698
Michael Stapelberg <michael@stapelberg.de>
2015-02-27 18:46:42 +0100

    fix coverage script by avoiding multiple imports of glog

--- coverage.sh
+++ coverage.sh
@@ -6,7 +6,7 @@
 INTERMEDIATE=$(mktemp)
 RESULT=$(mktemp)
 echo "mode: count" > "$RESULT"
-PACKAGES=$(go list github.com/robustirc/robustirc/... | grep -v /cmd/)
+PACKAGES=$(go list github.com/robustirc/robustirc/... | grep -v /cmd/ | grep -v mod_test | grep -v util | grep -v localnet)
 for pkg in $PACKAGES; do
     go test -covermode=count -coverprofile="$INTERMEDIATE" -coverpkg=$(echo $PACKAGES | tr ' ' ',') $pkg
     go run cmd/robustirc-merge-coverage/coverage.go -input="$INTERMEDIATE,$RESULT" -output="$RESULT"
