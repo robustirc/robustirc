3431e685fd57543f82d9efa86ade852be41d91d9
Michael Stapelberg <michael@stapelberg.de>
2015-05-11 23:26:59 +0200

    Implement SVSJOIN and SVSPART.
    fixes #119

--- ircserver/server_commands.go
+++ ircserver/server_commands.go
@@ -47,6 +47,8 @@ func init() {
 	commands["server_SVSNICK"] = &ircCommand{Func: (*IRCServer).cmdServerSvsnick, MinParams: 2}
 	commands["server_SVSMODE"] = &ircCommand{Func: (*IRCServer).cmdServerSvsmode, MinParams: 2}
 	commands["server_SVSHOLD"] = &ircCommand{Func: (*IRCServer).cmdServerSvshold, MinParams: 1}
+	commands["server_SVSJOIN"] = &ircCommand{Func: (*IRCServer).cmdServerSvsjoin, MinParams: 2}
+	commands["server_SVSPART"] = &ircCommand{Func: (*IRCServer).cmdServerSvspart, MinParams: 2}
 	commands["server_KILL"] = &ircCommand{Func: (*IRCServer).cmdServerKill, MinParams: 1}
 	commands["server_KICK"] = &ircCommand{Func: (*IRCServer).cmdServerKick, MinParams: 2}
 	commands["server_INVITE"] = &ircCommand{Func: (*IRCServer).cmdServerInvite, MinParams: 2}
@@ -407,6 +409,118 @@ func (i *IRCServer) cmdServerSvshold(s *Session, reply *Replyctx, msg *irc.Messa
 	} else {
 		delete(i.svsholds, nick)
 	}
+}
+
+func (i *IRCServer) cmdServerSvsjoin(s *Session, reply *Replyctx, msg *irc.Message) {
+	// SVSJOIN <nick> <chan>
+	nick := NickToLower(msg.Params[0])
+	channelname := msg.Params[1]
+
+	session, ok := i.nicks[nick]
+	if !ok {
+		i.sendServices(reply, &irc.Message{
+			Prefix:   i.ServerPrefix,
+			Command:  irc.ERR_NOSUCHNICK,
+			Params:   []string{msg.Prefix.Name, msg.Params[0]},
+			Trailing: "No such nick/channel",
+		})
+		return
+	}
+
+	if !IsValidChannel(channelname) {
+		i.sendServices(reply, &irc.Message{
+			Prefix:   i.ServerPrefix,
+			Command:  irc.ERR_NOSUCHCHANNEL,
+			Params:   []string{msg.Prefix.Name, channelname},
+			Trailing: "No such channel",
+		})
+		return
+	}
+	c, ok := i.channels[ChanToLower(channelname)]
+	if !ok {
+		c = &channel{
+			name:  channelname,
+			nicks: make(map[lcNick]*[maxChanMemberStatus]bool),
+		}
+		i.channels[ChanToLower(channelname)] = c
+	}
+	if _, ok := c.nicks[nick]; ok {
+		return
+	}
+	c.nicks[nick] = &[maxChanMemberStatus]bool{}
+	// If the channel did not exist before, the first joining user becomes a
+	// channel operator.
+	if !ok {
+		c.nicks[nick][chanop] = true
+	}
+	session.Channels[ChanToLower(channelname)] = true
+
+	i.sendChannel(c, reply, &irc.Message{
+		Prefix:   &session.ircPrefix,
+		Command:  irc.JOIN,
+		Trailing: channelname,
+	})
+	var prefix string
+	if c.nicks[nick][chanop] {
+		prefix = prefix + string('@')
+	}
+	i.sendServices(reply, &irc.Message{
+		Prefix:   i.ServerPrefix,
+		Command:  "SJOIN",
+		Params:   []string{"1", channelname},
+		Trailing: prefix + session.Nick,
+	})
+	// Integrate the topic response by simulating a TOPIC command.
+	i.cmdTopic(session, reply, &irc.Message{Command: irc.TOPIC, Params: []string{channelname}})
+	i.cmdNames(session, reply, &irc.Message{Command: irc.NAMES, Params: []string{channelname}})
+}
+
+func (i *IRCServer) cmdServerSvspart(s *Session, reply *Replyctx, msg *irc.Message) {
+	// SVSPART <nick> <chan>
+	nick := NickToLower(msg.Params[0])
+	channelname := msg.Params[1]
+
+	session, ok := i.nicks[nick]
+	if !ok {
+		i.sendServices(reply, &irc.Message{
+			Prefix:   i.ServerPrefix,
+			Command:  irc.ERR_NOSUCHNICK,
+			Params:   []string{msg.Prefix.Name, msg.Params[0]},
+			Trailing: "No such nick/channel",
+		})
+		return
+	}
+
+	c, ok := i.channels[ChanToLower(channelname)]
+	if !ok {
+		i.sendServices(reply, &irc.Message{
+			Prefix:   i.ServerPrefix,
+			Command:  irc.ERR_NOSUCHCHANNEL,
+			Params:   []string{msg.Prefix.Name, channelname},
+			Trailing: "No such channel",
+		})
+		return
+	}
+
+	if _, ok := c.nicks[nick]; !ok {
+		i.sendServices(reply, &irc.Message{
+			Prefix:   i.ServerPrefix,
+			Command:  irc.ERR_NOTONCHANNEL,
+			Params:   []string{msg.Prefix.Name, channelname},
+			Trailing: "You're not on that channel",
+		})
+		return
+	}
+
+	i.sendServices(reply, i.sendChannel(c, reply, &irc.Message{
+		Prefix:  &session.ircPrefix,
+		Command: irc.PART,
+		Params:  []string{channelname},
+	}))
+
+	delete(c.nicks, nick)
+	i.maybeDeleteChannel(c)
+	delete(session.Channels, ChanToLower(channelname))
 }
 
 func (i *IRCServer) cmdServerSvsmode(s *Session, reply *Replyctx, msg *irc.Message) {
--- ircserver/server_commands_test.go
+++ ircserver/server_commands_test.go
@@ -409,3 +409,66 @@ func TestServerQuit(t *testing.T) {
 		i.ProcessMessage(types.RobustId{}, ids["services"], irc.ParseMessage(":blorgh QUIT")),
 		":blorgh!enforcer@robust/0x13c6cdee3e749faf QUIT :")
 }
+
+func TestServerSvsjoin(t *testing.T) {
+	i, ids := stdIRCServerWithServices()
+
+	i.ProcessMessage(types.RobustId{}, ids["secure"], irc.ParseMessage("JOIN #test"))
+
+	mustMatchIrcmsg(t,
+		i.ProcessMessage(types.RobustId{}, ids["services"], irc.ParseMessage(":NickServ SVSJOIN bleh #test")),
+		irc.ParseMessage(":robustirc.net 401 NickServ bleh :No such nick/channel"))
+
+	msg := irc.ParseMessage(":NickServ SVSJOIN xeen #TEST")
+	msgid := types.RobustId{Id: time.Now().UnixNano()}
+	reply := i.ProcessMessage(msgid, ids["services"], msg)
+	i.SendMessages(reply, ids["services"], msgid.Id)
+	msgs, _ := i.Get(msgid)
+
+	mustMatchIrcmsgs(t,
+		&Replyctx{Messages: msgs},
+		[]*irc.Message{
+			irc.ParseMessage(":xeen!baz@robust/0x13b5aa0a2bcfb8af JOIN :#TEST"),
+			irc.ParseMessage(":robustirc.net SJOIN 1 #TEST :xeen"),
+			irc.ParseMessage(":robustirc.net 331 xeen #TEST :No topic is set"),
+			irc.ParseMessage(":robustirc.net 353 xeen = #TEST :@sECuRE xeen"),
+			irc.ParseMessage(":robustirc.net 366 xeen #TEST :End of /NAMES list."),
+		})
+
+	mustMatchInterestedMsgs(t, i,
+		msg, []*types.RobustMessage{msgs[0]},
+		[]types.RobustId{ids["secure"], ids["mero"], ids["xeen"]},
+		[]bool{true, false, true})
+
+	mustMatchInterestedMsgs(t, i,
+		msg, []*types.RobustMessage{msgs[1]},
+		[]types.RobustId{ids["secure"], ids["mero"], ids["xeen"], ids["services"]},
+		[]bool{false, false, false, true})
+
+	mustMatchInterestedMsgs(t, i,
+		msg, []*types.RobustMessage{msgs[2]},
+		[]types.RobustId{ids["secure"], ids["mero"], ids["xeen"]},
+		[]bool{false, false, true})
+
+	mustMatchIrcmsgs(t,
+		i.ProcessMessage(types.RobustId{}, ids["services"], irc.ParseMessage(":NickServ SVSJOIN xeen #bar")),
+		[]*irc.Message{
+			irc.ParseMessage(":xeen!baz@robust/0x13b5aa0a2bcfb8af JOIN :#bar"),
+			irc.ParseMessage(":robustirc.net SJOIN 1 #bar :@xeen"),
+			irc.ParseMessage(":robustirc.net 331 xeen #bar :No topic is set"),
+			irc.ParseMessage(":robustirc.net 353 xeen = #bar :@xeen"),
+			irc.ParseMessage(":robustirc.net 366 xeen #bar :End of /NAMES list."),
+		})
+
+	mustMatchIrcmsgs(t,
+		i.ProcessMessage(types.RobustId{}, ids["services"], irc.ParseMessage(":NickServ SVSPART xeen #bar")),
+		[]*irc.Message{
+			irc.ParseMessage(":xeen!baz@robust/0x13b5aa0a2bcfb8af PART #bar"),
+		})
+
+	mustMatchInterested(t, i,
+		ids["services"], irc.ParseMessage(":NickServ SVSPART xeen #test"),
+		[]types.RobustId{ids["secure"], ids["mero"], ids["xeen"], ids["services"]},
+		[]bool{true, false, true, true})
+
+}
