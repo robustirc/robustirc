robustirc/robustirc

commit 4eca5694e3c547130ddc178fb1a83a9f730a3408

Recreate the irclog database instead of wiping it clean.
See the comment for the (significant!) savings.

robustirc.go
@@ -692,20 +692,23 @@ func (fsm *FSM) Restore(snap io.ReadCloser) error {
 	log.Printf("Restoring snapshot\n")
 	defer snap.Close()
 
-	// Clear state by resetting the ircserver package’s state and deleting the
-	// entire ircstore. Snapshots contain the entire (possibly compacted)
-	// ircstore, so this is safe.
-	min, err := fsm.ircstore.FirstIndex()
+	if err := ircStore.Close(); err != nil {
+		log.Fatal(err)
+	}
+	// Deleting irclog and creating a new database is significantly faster than
+	// using DeleteRange() on the entire keyspace. Re-creating the database
+	// saves us 4 minutes of CPU time (out of 5 minutes total!) and >1G of
+	// memory usage.
+	irclogPath := filepath.Join(*raftDir, "irclog")
+	if err := os.RemoveAll(irclogPath); err != nil {
+		log.Fatal(err)
+	}
+	var err error
+	ircStore, err = raft_store.NewLevelDBStore(irclogPath, true)
 	if err != nil {
-		return err
+		log.Fatal(err)
 	}
-	max, err := fsm.ircstore.LastIndex()
-	if err != nil {
-		return err
-	}
-	if err := fsm.ircstore.DeleteRange(min, max); err != nil {
-		return err
-	}
+	fsm.ircstore = ircStore
 
 	ircServer = ircserver.NewIRCServer(*raftDir, *network, time.Now())
 

commit 22e731c8e33584e0a2c8b8f4c2e5649aa1433af2

fix compaction_test (commit 4eca569 broke it)

robustirc.go
@@ -692,7 +692,7 @@ func (fsm *FSM) Restore(snap io.ReadCloser) error {
 	log.Printf("Restoring snapshot\n")
 	defer snap.Close()
 
-	if err := ircStore.Close(); err != nil {
+	if err := fsm.ircstore.Close(); err != nil {
 		log.Fatal(err)
 	}
 	// Deleting irclog and creating a new database is significantly faster than

compaction_test.go
@@ -145,8 +145,8 @@ func TestCompaction(t *testing.T) {
 		t.Fatalf("fsm.Restore(): %v", err)
 	}
 
-	first, _ := ircstore.FirstIndex()
-	last, _ := ircstore.LastIndex()
+	first, _ := fsm.ircstore.FirstIndex()
+	last, _ := fsm.ircstore.LastIndex()
 
 	if last-first >= uint64(len(logs)) {
 		t.Fatalf("Compaction did not decrease log size. got: %d, want: < %d", last-first, len(logs))